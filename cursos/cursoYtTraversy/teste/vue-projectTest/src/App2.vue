3dc7ce921f24e67c988b1fda256b003a0328bfbe
Natan Braslavsky <natanbraslavsky1@gmail.com>
2026-01-14 19:43:01 -0300

    estudos vue.js

--- cursos/cursoYtTraversy/teste/vue-projectTest/src/App2.vue
+++ cursos/cursoYtTraversy/teste/vue-projectTest/src/App2.vue
@@ -1,25 +1,37 @@
-<!-- <script>
+<script setup>
 
-export default {
-  data() {
-    return {
-      name: 'Natan Pereira',
-      status: true,
-      tasks: ['Task One', 'Task Two', 'Task Three'],
-      link: 'https://google.com',
-    };
-  },
-  methods: {
-    changeStatus(){
-      if(this.status){
-        this.status = false;
-      }
-      else{
-        this.status = true;
-      }
-    }
+import { onMounted, ref } from 'vue';
+
+const name = ref("Natan Pereira");
+const status = ref(true);
+const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+const link = ref('https://google.com');
+const newTask = ref('');
+
+const changeStatus = () => {
+  status.value = !status.value;
+}
+
+const addTask = () => {
+  if(newTask.value.trim() !== ''){
+    tasks.value.push(newTask.value);
+    newTask.value = '';
   }
 }
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+}
+
+onMounted(async () => {
+  try{
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await response.json();
+    tasks.value = data.map((task) => task.title)
+  } catch (error){
+    console.log('Error')
+  }
+})
 
 </script>
 
@@ -27,12 +39,25 @@ export default {
   <h1>{{ name }}</h1>
   <p v-if="status">Status ok</p>
   <p v-else>Status no ok</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label><br>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
   <h3>Tasks:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="task" style="margin-top: 5px;">
+      <span>
+        {{ task }}
+      </span>
+      <button style="margin-left: 5px;" @click="deleteTask(index)">X</button>
+    </li>
   </ul>
   <a v-bind:href="link">Click for google</a>
+
   <br>
   <button @click="changeStatus">Change</button>
 </template>
- -->
+
